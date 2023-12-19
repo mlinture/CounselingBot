3046de031171a4536ffbfb6d3282dcb044600d27
Abel <abzajzon@gmail.com>
2023-12-19 14:01:45 -0800

    recent

--- counselingbot/pages/api/chat.js
+++ counselingbot/pages/api/chat.js
@@ -1,10 +1,38 @@
+const OPENAI_API_KEY = "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";
+
+// import OpenAI from "openai";
+
+// const openai = new OpenAI();
+
 export default async function handler(req, res) {
-    console.log("in the backend");
     const { userInput } = req.body;
 
     // implement logic to call OpenAI's API
+    try {
+      const openaiResponse = await fetch('https://api.openai.com/v1/engines/gpt-4/completions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${OPENAI_API_KEY}`
+        },
+        body: JSON.stringify({
+          prompt: userInput,
+          max_tokens: 150,
+        }),
+      });
 
-    // For now, we'll just return a placeholder response
-    res.status(200).json({ response: "OpenAI's response will go here" });
+      const openaiData = await openaiResponse.json();
+      console.log(openaiData);
+
+      res.status(200).json({ response: openaiData.choices[0].text });
+
+    }
+    catch (error) {
+      console.error('Error calling OpenAI:', error);
+      res.status(500).json({ error: 'Error calling OpenAI' });
+    }
+    
+    
+    
     
   }
